29cc3a3e0a3f53c5fb6c24f3d8d00f76efd6d2d7
yazılım <yazlm@192.168.1.22>
2016-03-08 23:37:45 +0200

    dene2

---
+++ deneme/src/deneme/Anonim.java
@@ -0,0 +1,5 @@
+package deneme;
+
+public interface Anonim {
+	public int call(int a, int b);
+}
--- deneme/src/deneme/main.java
+++ deneme/src/deneme/main.java
@@ -1,9 +1,19 @@
 package deneme;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int fonk(int a, int b, Anonim anonim) {
+		   return anonim.call(a, b) * 2;
+		};
+	
+		
+	public static void main(String[] args) throws IOException {
+		
+	System.out.println(fonk(2, 3, (a, b) -> a + b));	 
 
 	}
 
